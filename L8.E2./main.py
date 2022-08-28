8f646f05269a3526504e569e9720df73693b213b
Roberdr <amigo.rdiaz@gmail.com>
2022-08-28 23:04:16 +0200

    Update main.py

--- L8.E2./main.py
+++ L8.E2./main.py
@@ -1,1 +1,25 @@
+import pickle
 
+class Vehiculo():
+    ruedas = 0
+    combustible = None
+
+    def __init__(self, ruedas, combustible) -> None:
+        self.ruedas = ruedas
+        self.combustible = combustible
+
+    
+v1 = Vehiculo(4, 'Gasolina')
+
+
+
+f=open('fichero.bin', 'wb')
+pickle.dump(v1, f)
+f.close()
+
+f=open('fichero.bin', 'rb')
+v2=pickle.load(f)
+f.close()
+
+print(v2.ruedas)
+print(v2.combustible)
